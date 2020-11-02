9f2ff314a56238d307b5c3b51b139630cee28108
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-02 16:18:25 -0500

    php basics covered

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,14 @@
-This is our completely amazing custom theme.
+<?php
+
+function greet($name, $colour) {
+  echo "<p>Hi, my name is $name and my favourite colour is $colour.</p>";
+
+}
+
+greet("John", "blue");
+greet("Jane", "green");
+
+?>
+<!-- bloginfo is a built in wordpress fn -->
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
