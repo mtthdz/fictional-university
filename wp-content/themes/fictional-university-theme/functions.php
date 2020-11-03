b9e8834deb035172f6a8dbd613b257b398d74091
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-03 15:15:52 -0500

    small page template edit

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -9,9 +9,14 @@ function university_files() {
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 
+function university_features() {
+  add_theme_support('title-tag');
+}
+
 // parameters: first when to call the fn, and then the fn
 // we dont use () for university_files because we're not running it here and now
 // php will handle the fn run
 add_action('wp_enqueue_scripts', 'university_files');
+add_action('after_setup_theme', 'university_features');
 
 ?>
