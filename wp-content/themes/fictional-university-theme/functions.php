0f1708586c0b83f65e288537b154648ef604db62
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-24 14:57:48 -0500

    created custom post type for events

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -6,6 +6,7 @@ function university_files() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   
+  // for webpack
   if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
@@ -23,10 +24,9 @@ function university_features() {
   add_theme_support('title-tag');
 }
 
+
 // parameters: first when to call the fn, and then the fn
 // we dont use () for university_files because we're not running it here and now
 // php will handle the fn run
 add_action('wp_enqueue_scripts', 'university_files');
 add_action('after_setup_theme', 'university_features');
-
-?>
