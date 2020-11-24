c90af394b693d26ed4975f8fc5e0f4159e5e7634
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-24 16:16:47 -0500

    added dynamic post excerpts to homepage

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,9 @@
 
 function university_post_types() {
   register_post_type('event', array(
+    // we need to add rest api as the new block editor uses js; without this, we'd have the classic block editor
+    'show_in_rest' => true, 
+    'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'events'),
     'has_archive' => true,
     'public' => true,
