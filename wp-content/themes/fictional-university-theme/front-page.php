3945bd52d1482b540014d2f3519ae1c3ab25ad41
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-24 16:31:11 -0500

    added custom fields plugin to use for events post type

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -26,8 +26,20 @@
               $homepageEvents->the_post(); // get all event posts loaded ?>
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month">Apr</span>
-                  <span class="event-summary__day">02</span>
+                  <span class="event-summary__month">
+                    <?php
+                      // the event_date param is from our custom fields plugin
+                      $eventDate = new DateTime(get_field('event_date'));
+                      echo $eventDate->format('M');
+                    ?>
+                  </span>
+                  <span class="event-summary__day">
+                    <?php
+                      // the event_date param is from our custom fields plugin
+                      $eventDate = new DateTime(get_field('event_date'));
+                      echo $eventDate->format('d');
+                    ?>                  
+                  </span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
