0b62d2020c60f4893bbfe79294d83c630f128881
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-26 16:57:37 -0500

    created programs post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,9 @@
 <?php
 
 function university_post_types() {
+
+
+  // event post type
   register_post_type('event', array(
     // we need to add rest api as the new block editor uses js; without this, we'd have the classic block editor
     'show_in_rest' => true, 
@@ -17,6 +20,25 @@ function university_post_types() {
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
+
+
+  // program post type
+  register_post_type('program', array(
+    // we need to add rest api as the new block editor uses js; without this, we'd have the classic block editor
+    'show_in_rest' => true, 
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Programs'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));  
 }
 
 add_action('init', 'university_post_types');
