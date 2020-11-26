7e29257f591fdf8e17639a7cc2528aa6d14f4b83
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-26 16:35:45 -0500

    created pagination for past events archive

--- wp-content/themes/fictional-university-theme/page-past-events.php
+++ wp-content/themes/fictional-university-theme/page-past-events.php
@@ -17,6 +17,7 @@ get_header(); ?>
 
     $today = date('Ymd');
     $pastEvents = new WP_Query(array(
+      'paged' => get_query_var('paged', 1),
       'post_type' => 'event',
       'meta_key' => 'event_date',
       // you need to declare meta_key to use meta_value
@@ -58,8 +59,10 @@ get_header(); ?>
       </div>      
     <?php }
 
-    echo paginate_links();
-
+    echo paginate_links(array(
+      'total' => $pastEvents->max_num_pages
+    ));
+    
   ?>
 
 </div>
