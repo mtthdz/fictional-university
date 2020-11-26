70508ff4bd7aac20f4dc61ce48cd9ae286a344e5
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-26 17:22:19 -0500

    added list of related events to program page

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -29,6 +29,21 @@
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
+
+      <?php
+        $relatedPrograms = get_field('related_programs');
+
+        if($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+          echo '<u class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+      ?>
+
     </div>
 
   <?php }
