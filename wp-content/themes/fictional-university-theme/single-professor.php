13947464c5680705aff47a963e6493ba4b3a2232
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-29 17:35:35 -0500

    updated dr barksalot cover photo

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -9,11 +9,14 @@
     // wordpress specific function to get all info of the next post
     the_post(); ?>
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div 
+        class="page-banner__bg-image" 
+        style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['url']; ?>);">
+      </div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Come back to this</p>
+          <p><?php the_field('page_banner_subtitle'); ?></p>
         </div>
       </div>  
     </div>   
