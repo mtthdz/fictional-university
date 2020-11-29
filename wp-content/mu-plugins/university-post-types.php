6723e52d99316f627e94a36a8f84af03822f5c20
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-29 16:38:26 -0500

    added professor post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -39,6 +39,23 @@ function university_post_types() {
     ),
     'menu_icon' => 'dashicons-awards'
   ));  
+
+
+  // professor post type
+  register_post_type('professor', array(
+    // we need to add rest api as the new block editor uses js; without this, we'd have the classic block editor
+    'show_in_rest' => true, 
+    'supports' => array('title', 'editor'),
+    'public' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professor'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more'
+  ));    
 }
 
 add_action('init', 'university_post_types');
