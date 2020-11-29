addd4f711ae7f4dacbdc8d7ac7f8d8daa5be75f5
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-29 16:24:56 -0500

    fixed footer list area under single event. added related events list to single program

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -36,7 +36,7 @@
         if($relatedPrograms) {
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
-          echo '<u class="link-list min-list">';
+          echo '<ul class="link-list min-list">';
           foreach($relatedPrograms as $program) { ?>
             <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
           <?php }
