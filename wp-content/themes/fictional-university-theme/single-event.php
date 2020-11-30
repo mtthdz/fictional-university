a1bb2e40c2e0dc1130c2fc81d918ac2e079b5fcb
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-30 13:14:12 -0500

    removed all hardcoded banners, replaced with pageBanner fn

--- wp-content/themes/fictional-university-theme/single-event.php
+++ wp-content/themes/fictional-university-theme/single-event.php
@@ -7,16 +7,10 @@
   // wordpress specific parameter
   while(have_posts()) {
     // wordpress specific function to get all info of the next post
-    the_post(); ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Come back to this</p>
-        </div>
-      </div>  
-    </div>   
+    the_post(); 
+    pageBanner();
+    ?>
+ 
 
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
