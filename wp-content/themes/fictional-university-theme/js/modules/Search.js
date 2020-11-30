b535b353652717553ac64fd49df494c4f7986524
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-30 14:54:04 -0500

    added keypress timeout fn to class component

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -5,8 +5,10 @@ class Search {
     this.openButton = $('.js-search-trigger');
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
+    this.searchField = $('#search-term');
     this.events();
     this.isOverlayOpen = false;
+    this.typingTimer;
   }
 
   // events
@@ -16,6 +18,7 @@ class Search {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
     $(document).on('keydown', this.keyPressDispatcher.bind(this));
+    this.searchField.on('keydown', this.typingLogic.bind(this));
   }
 
   // methods
@@ -37,6 +40,15 @@ class Search {
     } else if(e.keyCode === 27 && this.isOverlayOpen) {
       this.closeOverlay();
     }
+  }
+
+  typingLogic() {
+    // this will make the timer count only when the last key was pressed
+    // this will prevent auto searching every keypress event
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(function() {
+      console.log('nice');
+    }, 1000);
   }
 }
 
