6bd34854cc3b45a16302aa2b46060eb2229b83ee
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-01 16:27:20 -0500

    wrote in program to professor relationship in search results

--- wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -1,18 +1,15 @@
-<?php 
+<?php
 
 function universityRegisterSearch() {
-  // 3 args: namespace (i.e. /wp/v2/), route, and an array 
   register_rest_route('university/v1', 'search', array(
-    'methods' => WP_REST_SERVER::READABLE, // safer to use than just 'GET'
+    'methods' => WP_REST_SERVER::READABLE,
     'callback' => 'universitySearchResults'
   ));
 }
 
 function universitySearchResults($data) {
   $mainQuery = new WP_Query(array(
-    'post_type' => array('post', 'page', 'professor', 'program', 'event'),
-    // s is for search
-    // $data is what the user is looking up in the search bar
+    'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
     's' => sanitize_text_field($data['term'])
   ));
 
@@ -20,52 +17,77 @@ function universitySearchResults($data) {
     'generalInfo' => array(),
     'professors' => array(),
     'programs' => array(),
-    'events' => array()
+    'events' => array(),
+    'campuses' => array()
   );
 
   while($mainQuery->have_posts()) {
     $mainQuery->the_post();
-    
-    if(get_post_type() == 'post' OR get_post_type() == 'page') {
+
+    if (get_post_type() == 'post' OR get_post_type() == 'page') {
       array_push($results['generalInfo'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
         'postType' => get_post_type(),
         'authorName' => get_the_author()
       ));
-    } else if(get_post_type() == 'professor') {
+    }
+
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'program') {
+      array_push($results['programs'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'campus') {
+      array_push($results['campuses'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+
+    if (get_post_type() == 'event') {
+      array_push($results['events'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      ));
+    }
+    
+  }
+
+  $programRelationshipQuery = new WP_Query(array(
+    'post_type' => 'professor',
+    'meta_query' => array(
+      array(
+        'key' => 'related_programs',
+        'compare' => 'LIKE',
+        'value' => '"52'
+      )
+    )
+  ));
+
+  while($programRelationshipQuery->have_posts()) {
+    $programRelationshipQuery->the_post();
+
+    if(get_post_type() == 'professor') {
       array_push($results['professors'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
         'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
       ));
-    } else if(get_post_type() == 'program') {
-      array_push($results['programs'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink()
-      ));
-    } else if(get_post_type() == 'event') {
-      $eventDate = new DateTime(get_field('event_date'));
-      $description = null;
-      if(has_excerpt()) {
-        $description = get_the_excerpt();
-      } else {
-        $description = wp_trim_words(get_the_content(), 18);
-      }
-
-      array_push($results['events'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'month' => $eventDate->format('M'),
-        'day' => $eventDate->format('d'),
-        'description' => $description
-      ));
     }
   }
 
   return $results;
+
 }
 
 add_action('rest_api_init', 'universityRegisterSearch');
-
-?>
