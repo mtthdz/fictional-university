a2df9dbb3770f7dd45ade3589235d7f7866551b1
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-01 16:07:35 -0500

    completed events search results

--- wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -88,7 +88,7 @@ class Search {
 
           <div class="one-third">
             <h2 class="search-overlay__section-title">Programs</h2>
-            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No results found. <a href="${universityData.root_url}/programs">View  all programs</a></p>`}
+            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No results found. <a href="${universityData.root_url}/programs">View all programs</a></p>`}
               ${results.programs.map(item => `<li><a href=${item.link}>${item.title}</a></li>`).join('')}
             ${results.programs.length ? '</ul>' : '' }
 
@@ -107,7 +107,18 @@ class Search {
 
           <div class="one-third">
             <h2 class="search-overlay__section-title">Events</h2>
-
+            ${results.events.length ? '' : `<p>No results found. <a href="${universityData.root_url}/events">View all events</a></p>`}
+              ${results.events.map(item => `
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="${item.permalink}">
+                    <span class="event-summary__month">${item.month}</span>
+                    <span class="event-summary__day">${item.day}</span>
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="${item.permalink}">${item.title}</a></h5>
+                    <p>${item.description}<a href="${item.permalink}" class="nu gray">Learn more</a></div>
+                </div>
+              `).join('')}
           </div>
         </div>
       `);
