eaac6a52b37663d1919fc1f038e55e41121079e9
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-02 16:02:19 -0500

    created templates for traditional search page

--- wp-content/themes/fictional-university-theme/search.php
+++ wp-content/themes/fictional-university-theme/search.php
@@ -4,7 +4,7 @@ get_header();
 pageBanner(array(
   'title' => 'Search Results',
   // get_search_query turns all text into strings, preventing any cross site script attacks
-  'subtitle' => 'You searched for &ldquo;' . get_search_query() . '&rdquo;'
+  'subtitle' => 'You searched for &ldquo;' . esc_html(get_search_query(false)) . '&rdquo;'
 ))
 ?>
 
@@ -12,20 +12,9 @@ pageBanner(array(
 <div class="container container--narrow page-section">
   <?php
     while(have_posts()) {
-      the_post(); ?>
-      <div class="post-item">
-        <h2 class="headline headline--medium headline--post-title">
-          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h2>
-        <div class="metabox">
-          <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-        </div>
-        <div class="generic-content">
-          <?php the_excerpt(); ?>
-          <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">continue reading &raquo;</a></p>
-        </div>
-      </div>
-    <?php }
+      the_post(); 
+      get_template_part('template-parts/content', get_post_type());  
+    }
 
     echo paginate_links();
 
