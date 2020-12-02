224885873a95200adde4cb5abce6b011feb03692
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-02 16:08:40 -0500

    templated search fn component

--- wp-content/themes/fictional-university-theme/search.php
+++ wp-content/themes/fictional-university-theme/search.php
@@ -11,13 +11,20 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
   <?php
-    while(have_posts()) {
-      the_post(); 
-      get_template_part('template-parts/content', get_post_type());  
+
+    if(have_posts()) {
+      while(have_posts()) {
+        the_post(); 
+        get_template_part('template-parts/content', get_post_type());  
+      }
+
+      echo paginate_links();
+
+    } else {
+      echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
     }
 
-    echo paginate_links();
-
+    get_search_form();
   ?>
 
 </div>
---
+++ wp-content/themes/fictional-university-theme/searchform.php
@@ -0,0 +1,8 @@
+<!-- esc_url is a security measure for urls -->
+<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+  <label class="headline headline--medium" for="s">Perform a New Search:</label>
+  <div class="search-form-row">
+    <input class="s" id="s" type="search" name="s" placeholder="what are you looking for?">
+    <input class="search-submit" type="submit" value="Search">
+  </div>  
+</form>
