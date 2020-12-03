00c2613c2fee83b0b0c0388e26576ba6341e5468
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-03 15:36:40 -0500

    added edit functionality to notes

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -7,20 +7,22 @@ class MyNotes {
 
   events() {
     $('.delete-note').on('click', this.deleteNote)
+    $('.edit-note').on('click', this.editNote)
   }
 
 
   // methods
-  deleteNote() {
+  deleteNote(e) {
+    const thisNote = $(e.target).parents('li');
+    
     $.ajax({
       beforeSend: (xhr) => {
         xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
       },
-      url: universityData.root_url + `/wp-json/wp/v2/note/82`,
+      url: universityData.root_url + `/wp-json/wp/v2/note/${thisNote.data('id')}`,
       type: 'DELETE',
       success: () => {
-        console.log('nice');
-        console.log(response);
+        thisNote.slideUp();
       },
       error: () => {
         console.log('nope'),
@@ -28,6 +30,14 @@ class MyNotes {
       }
     })
   }
+
+  editNote(e) {
+    const thisNote = $(e.target).parents('li');
+
+    thisNote.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field');
+    thisNote.find('.update-note').addClass('update-note--visible');
+  }
+  
 }
 
 export default MyNotes;
