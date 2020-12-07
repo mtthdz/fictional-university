2563bf405b94900837ec38bdafdd77308f22063d
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-07 16:43:00 -0500

    added event handler on clicking like buttons

--- wp-content/themes/fictional-university-theme/js/modules/Like.js
+++ wp-content/themes/fictional-university-theme/js/modules/Like.js
@@ -1,12 +1,32 @@
+import $ from 'jquery';
+
 class like {
   constructor() {
+    this.events();
   }
 
   events() {
-
+    $('.like-box').on('click', this.ourClickDispatcher.bind(this));
   }
 
   // methods
+  ourClickDispatcher(e) {
+    const currentLikeBox = $(e.target).closest('.like-box');
+
+    if(currentLikeBox.data('exists') == 'yes') {
+      this.deleteLike();
+    } else {
+      this.createLike();
+    }
+  }
+
+  createLike() {
+    console.log('nice');
+  }
+
+  deleteLike() {
+    console.log('not nice');
+  }
 }
 
-export default ike;
+export default like;
